e8c6c1bb13e147b06c5c368faa811257dcf0138d
developermithu <developermithu@gmail.com>
2023-02-07 21:56:40 +0600

    initial commit

---
+++ src/components/LoaderIcon.tsx
@@ -0,0 +1,9 @@
+export const LoaderIcon = () => {
+  return (
+    <div className="la-ball-beat text-xs absolute top-5 lg:top-6 right-5">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Image Generator with Nextjs 13 & Openai | Developed by DeveloperMithu</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
